876391fa6d5e6a7c0ebbdd72f1d71458ef4d4679
NouranAmr <nouranamr206@gmail.com>
2019-04-23 15:49:29 +0200

    Add IOSLifeCycle App

---
+++ IOSLifeCycle/IOSLifeCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSLifeCycle
+//
+//  Created by JETS Mobile Lab-3 on 4/13/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
