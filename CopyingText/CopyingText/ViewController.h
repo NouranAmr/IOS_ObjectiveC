d127ea980d731825f8b5fd3fea639078c9d7d422
NouranAmr <nouranamr206@gmail.com>
2019-04-23 15:45:53 +0200

    Add CopyingText App

---
+++ CopyingText/CopyingText/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  CopyingText
+//
+//  Created by JETS Mobile Lab-3 on 4/13/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *showlbl;
+@property (strong, nonatomic) IBOutlet UITextField *entertxt;
+
+@property (strong, nonatomic) IBOutlet UIButton *sendbtn;
+
+@end
+
---
+++ CopyingText/CopyingText/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  CopyingText
+//
+//  Created by JETS Mobile Lab-3 on 4/13/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    _showlbl.text=@"press the button";
+    
+}
+- (IBAction)sendbtn:(id)sender {
+    _showlbl.text=_entertxt.text;
+}
+
+
+@end
